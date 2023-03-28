3fc6279457587bfd25459d1c1e267aef0a4848b6
okayaparna <krisa304@newschool.edu>
2023-03-28 17:40:17 -0400

    .

--- home.js
+++ home.js
@@ -135,7 +135,7 @@ function dragElement(elmnt) {
 
 // gum--
 
-dragElement(document.getElementById("gum-wrapper"));
+dragElement(document.getElementById("photo-wrapper"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
